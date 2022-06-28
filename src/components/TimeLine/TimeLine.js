110127918720dc505ef690e437b4ec82997f6dd8
aroramrinaal <aroramrinaal@gmail.com>
2022-06-28 15:49:24 +0530

    feat: changes.

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -46,7 +46,7 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-      The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+      I started my coding journey in June of 2022 and since then I have been exploring technologies like javascript and cloud-native tools. Also trying to be part of awesome open-source communities.
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
